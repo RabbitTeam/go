01796bf07051dd933b57c951ac1ff5002a2b0360
majian <majian159@live.com>
2018-02-11 17:53:47 +0800

    add dingtalk recipe build settings.

--- build/settings.ps1
+++ build/settings.ps1
@@ -4,9 +4,11 @@ $artifacts_dir = "$base_dir\artifacts"
 $build_dir = "$base_dir\build"
 $source_dir = "$base_dir\src"
 $working_dir = "$build_dir\working"
+$recipe_dir = "$base_dir\recipes"
 $nuget_source = "https://api.nuget.org/v3/index.json"
 $nuget_apiKey = "xxxxxxxxxx"
 $projects = @(
 	@{ Name = "Rabbit.Go.Abstractions"; SourceDir = "$source_dir\Rabbit.Go.Abstractions"; ExternalNuGetDependencies = $null; UseMSBuild = $False; },
-	@{ Name = "Rabbit.Go.Core"; SourceDir = "$source_dir\Rabbit.Go.Core"; ExternalNuGetDependencies = $null; UseMSBuild = $False; }
+	@{ Name = "Rabbit.Go.Core"; SourceDir = "$source_dir\Rabbit.Go.Core"; ExternalNuGetDependencies = $null; UseMSBuild = $False; },
+	@{ Name = "Rabbit.Go.DingTalk"; SourceDir = "$recipe_dir\DingTalk"; ExternalNuGetDependencies = $null; UseMSBuild = $False; }
 )
